ec7acf0591bbcb71c8cdde38e863571c88d9db20
NovikovIlya <91413531+NovikovIlya@users.noreply.github.com>
2023-09-18 10:58:39 +0300

    02

--- authController.js
+++ authController.js
@@ -1,37 +1,38 @@
-const User = require('./models/User')
-const Role = require('./models/Role')
+const User = require('./models/User');
+const Role = require('./models/Role');
 const bcrypt = require('bcryptjs');
 
 class authController {
-    async registration(req,res){
-        try {
-            const {username,password} = req.body;
-            const candidate = await User.findOne({username})
-            if(candidate){
-                return res.status(400).json({message:'Пользователь уже существует'})
-            }
-            const hashPassword = bcrypt.hashSync(password, salt);
-            const user = new User({username,})
-        } catch (error) {
-            console.log(e)
-            res.status(400).json({message:'Registration error'})
-        }
+  async registration(req, res) {
+    try {
+      const { username, password } = req.body;
+      const candidate = await User.findOne({ username });
+      if (candidate) {
+        return res.status(400).json({ message: 'Пользователь уже существует' });
+      }
+      const hashPassword = bcrypt.hashSync(password, 7);
+      const userRole = await Role.findOne({ value: 'USER' });
+
+      const user = new User({ username, password: hashPassword, roles: [userRole.value] });
+
+      await user.save();
+      return res.json({ message: 'Пользователь успешно зарегестрирован' });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ message: 'Registration error' });
     }
-    async login(req,res){
-        try {
-            
-        } catch (error) {
-            console.log(e)
-            res.status(400).json({message:'Login error'})
-        }
+  }
+  async login(req, res) {
+    try {
+    } catch (error) {
+      console.log(e);
+      res.status(400).json({ message: 'Login error' });
     }
-    async getUser(req,res){
-        try {
- 
-            res.json('server work')
-        } catch (error) {
-            
-        }
-    }
+  }
+  async getUser(req, res) {
+    try {
+      res.json('server work');
+    } catch (error) {}
+  }
 }
-module.exports = new authController()
+module.exports = new authController();
