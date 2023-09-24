5ae0ffd79b4948e09701893d7713d61032721fe6
NovikovIlya <91413531+NovikovIlya@users.noreply.github.com>
2023-09-24 21:07:21 +0300

    41

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const PORT = process.env.PORT || 5000;
 const cors = require('cors')
 
 const app = express();
-app.use(cors())
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 const authRouter = require('./authRouter');
 app.use(express.json());
 app.use("/auth", authRouter);
