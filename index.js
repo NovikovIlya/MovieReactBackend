fdb761294c5416770ccb7906cac716e3d95b0ed8
NovikovIlya <91413531+NovikovIlya@users.noreply.github.com>
2023-09-24 21:02:44 +0300

    40

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 const cors = require('cors')
+
 const app = express();
 app.use(cors())
 const authRouter = require('./authRouter');
